7e3bd62f789fe945008bcceef67f144369618ab7
cuevas69 <andrescuevas507@gmail.com>
2023-10-24 10:54:14 +0200

    andres

--- afuego/app/build.gradle.kts
+++ afuego/app/build.gradle.kts
@@ -40,5 +40,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
 }
